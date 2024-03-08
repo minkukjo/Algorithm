55d06f595264ced12465f6d79e6e1fe2dcff4a82
minkukjo <strike0115@naver.com>
2024-03-09 00:01:17 +0900

    양궁대회

--- preparing-codingtest/backtracking.py
+++ preparing-codingtest/backtracking.py
@@ -19,8 +19,6 @@ def solution(k, dungeons):
 
 # N-퀸
 def solution(n):
-
-
     arr = [0] *n
     def isExistQueen(position):
         for i in range(position):
@@ -40,3 +38,49 @@ def solution(n):
         return answer
 
     return dfs(0)
+
+# 양궁 대회
+
+def solution(n, info):
+    answer = []
+    ryan = [0 for i in range(11)]
+    max_score = [0]
+
+    def dfs(arrow, ryan, index):
+        if arrow == 0:
+            ryan_score = 0
+            apeach_score = 0
+            for i in range(11):
+                if ryan[i] == 0 and info[i] == 0:
+                    continue
+
+                if ryan[i] > info[i]:
+                    ryan_score += (10-i)
+                else:
+                    apeach_score += (10-i)
+            if ryan_score > apeach_score:
+                dif = ryan_score - apeach_score
+
+                if dif > max_score[0]:
+                    max_score[0] = dif
+                    answer = list(ryan)
+                elif dif == max_score[0]:
+                    for i in range(11):
+                        if ryan[-1] > answer[-1]:
+                            answer = list(ryan)
+                            return
+                        else:
+                            return
+        if index == 11:
+            return
+        for i in range(index, n+1):
+            if i <= n:
+                ryan[index] = i
+                dfs(arrow-i,ryan, index+1)
+                ryan[index] = 0
+    
+    dfs(n, ryan, 0)
+
+    return answer
+
+print(solution(5, [2,1,1,1,0,0,0,0,0,0,0]))
